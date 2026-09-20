e54d85e2480f4f029f1e93a30c3e3e76faef6137
i-brandao <ibrandao2410@gmail.com>
2026-09-19 23:50:04 -0300

    V0.3 - adiciona histórico, resumo e validações

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static double pesoGeral = 0.0;
+    public static ArrayList<Double> historicoPesos = new ArrayList<>();
     public static double alturaGeral = 0.0;
     public static String observacao = "";
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        String apresentacao = "Essa e a versão 0.2";
+        String apresentacao = "Essa e a versão 0.3";
 
         while (true) {
             for (int i = 0; i <= 22; i++) {
@@ -24,11 +25,11 @@ public class Main {
                 System.out.printf("=");
             }
 
-            System.out.println("\nO que deseja fazer?" + "\n[1]Fazer cadastro [2]Registrar medição [3]Calcular IMC [4]ver resumo [5]Sair");
+            System.out.println("\nO que deseja fazer?" + "\n[1]Fazer cadastro [2]Registrar medição [3]Calcular IMC [4]ver resumo [5]Ver Histórico [6]Sair");
             int escolha = input.nextInt();
             input.nextLine();
 
-            if  (escolha == 5) {
+            if (escolha == 6) {
                 System.out.println("Volte sempre!");
                 System.out.println(apresentacao);
                 break;
@@ -51,6 +52,9 @@ public class Main {
                 case 4:
                     mostrarResumo();
                     break;
+                case 5:
+                    mostrarHistorico();
+                    break;
                 default:
                     System.out.println("opção inválida, tente novamente!");
             }
@@ -58,14 +62,14 @@ public class Main {
     }
 
     public static void retornarImc() {
-        if(alturaGeral == 0 || pesoGeral == 0) {
+        if (alturaGeral != 0.0 && !historicoPesos.isEmpty()) {
+            double ultimopeso = historicoPesos.get(historicoPesos.size() - 1);
+            double imc = ultimopeso / (alturaGeral * alturaGeral);
+            System.out.printf("IMC atual: %.2f\n", imc);
+        } else {
             System.out.println("Erro: Você precisa primeiro fazer o cadastro (altura) e registrar uma medição (peso)!");
-            return;
         }
-        double imc = pesoGeral / (alturaGeral *  alturaGeral);
-        System.out.println("IMC: " + imc);
     }
-
     public static void cadastrarUsuario(Scanner input) {
 
         while(true){
@@ -104,41 +108,63 @@ public class Main {
     }
 
     public static void registrarMedicao(Scanner input) {
-        while(true){
+        while (true) {
+
+            if (alturaGeral <= 0) {
+                System.out.println("É necessário fazer o cadastro primeiro!");
+                break;
+            }
+
             System.out.println("Digite seu peso (em kg): ");
             double pesodigitado = input.nextDouble();
+
+            if (pesodigitado <= 0) {
+                System.out.println("Peso inválido, tente novamente!");
+                continue;
+            }
+
+            historicoPesos.add(pesodigitado);
 
             System.out.println("Deseja registrar alguma observação? [1]sim [2]nao");
             int confirmacao = input.nextInt();
             input.nextLine();
 
-            if(confirmacao == 1){
+            if (confirmacao == 1) {
                 System.out.println("Digite a observação: ");
                 observacao = input.nextLine();
-                pesoGeral = pesodigitado;
-                break;
             }
-            else {
-                pesoGeral =  pesodigitado;
-                break;
-            }
+
+            break;
         }
     }
 
     public static void mostrarResumo() {
 
-        if(alturaGeral == 0 || pesoGeral == 0) {
-            System.out.println("Erro: Você precisa primeiro fazer o cadastro e resgistrar uma medição!!!");
-            return;
+        if(alturaGeral != 0.0 && !historicoPesos.isEmpty()) {
+            double ultimopeso = historicoPesos.get(historicoPesos.size() - 1);
+            double imc = ultimopeso / (alturaGeral * alturaGeral);
+
+            System.out.println("\n========== RESUMO ==========");
+            System.out.println("Peso: " + ultimopeso + "kgs");
+            System.out.println("IMC: " + imc);
+            System.out.println("Data: " + LocalDate.now());
+            System.out.println("observacao: " + observacao);
+            System.out.println("============================");
         }
-
-        double imc = pesoGeral  / (alturaGeral *  alturaGeral);
-
-        System.out.println("\n========== RESUMO ==========");
-        System.out.println("Peso: " + pesoGeral + "kgs");
-        System.out.println("IMC: " + imc);
-        System.out.println("Data: " + LocalDate.now());
-        System.out.println("observacao: " + observacao);
-        System.out.println("============================");
+        else {
+            System.out.println("Erro: Você precisa primeiro fazer o cadastro e registrar uma medição!!!");
+        }
+    }
+    public static void mostrarHistorico() {
+        if(historicoPesos.isEmpty()) {
+            System.out.println("É necessário fazer uma primeira medição");
+        }
+        else {
+            System.out.println("========== HISTORICO ==========");
+            for(int i = 0; i < historicoPesos.size(); i++) {
+                System.out.println("Peso: " + historicoPesos.get(i) + " kg");
+            }
+            System.out.println("============================");
+        }
     }
 }
